3c44047c65e5bb87b4d880f958c8ad15c7b4f540
khushisahoo <khushisahoo0246@gmail.com>
2022-01-05 23:14:03 +0530

    bringing redux State into homescreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,32 @@
-import React , {useState , useEffect} from 'react';
+import React , {useEffect} from 'react';
 import{Row , Col , Container} from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import {useDispatch , useSelector} from 'react-redux';
+import { listProducts } from '../actions/productAction';
 const HomeScreen = () => {
-    const [products , setProducts]= useState([]);
-
+      const dispatch = useDispatch();
+      const productList = useSelector(state => state.productList)
+      const {loading , error , products} = productList
     useEffect(()=>{
-        const fetchProducts = async()=>{
-        const {data}=  await axios.get('/api/products')
-        setProducts(data)
-        }
-        fetchProducts()
-    } , [])
+        dispatch(listProducts())
+    } , [dispatch])
     
     return (
         <>  
         <main className="py-3">
         <Container>
         <h4>Latest Product</h4>
-            <Row>
+        {loading ?
+         <h2>Loading...</h2>
+         : error? <h3>{error}</h3>
+         : <Row>
                 {products.map(product=>{
                     return(<Col key={product._id} sm={12} lg={4} xl={3}>
                         <Product product={product}/>
                     </Col>)
                 })}
-            </Row>
+            </Row>}
+           
         </Container>
         </main>
         </>
