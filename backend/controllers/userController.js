ca9c16ed7d06f284326c7e4cab732c3a2ec403a4
khushisahoo <khushisahoo0246@gmail.com>
2022-01-18 22:31:06 +0530

    generating json web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,6 @@
 import AsyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-import Product from '../models/userModel.js';
-
+import generateToken from "../utils/generateToken.js";
 
 // Auth the user and grt token
 // POST/api/users/login
@@ -17,7 +16,7 @@ const authUser = AsyncHandler(async(req , res) => {
            name:user.name,
            email:user.email,
            isAdmin:user.isAdmin,
-           token:null
+           token:generateToken(user._id)
        })
     }else{
         res.status(401)
