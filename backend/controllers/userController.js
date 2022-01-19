feda359b514cb3c803ac59cddbe5bcda1e8863f3
khushisahoo <khushisahoo0246@gmail.com>
2022-01-19 12:23:16 +0530

    user registration and password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,5 +24,56 @@ const authUser = AsyncHandler(async(req , res) => {
     }
 
 })
+// register the user 
+// POST/api/users
+//public route
 
-export {authUser};
+const registerUser = AsyncHandler(async(req , res) => {
+    const {name , email , password} = req.body;
+    res.send({email , password});
+    const userExists= await User.findOne({email:email});
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name, 
+        email,
+        password
+    })
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+           name:user.name,
+           email:user.email,
+           isAdmin:user.isAdmin,
+           token:generateToken(user._id)
+        })
+    }else{
+       res.status(400)
+       throw new Error('Invalid User Data')
+    }
+})
+// Get user profile
+// GET/api/users/profile
+//private route
+
+const getUserProfile = AsyncHandler(async(req , res) => {
+    const user= await User.findById(req.user._id);
+    if(user){
+        res.json({
+            _id: user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin
+        })
+
+    }else{
+        res.status(404);
+        throw new Error("user not found")
+    }
+
+})
+
+export {authUser , getUserProfile , registerUser};
