94cbd35cfa16e8bd316ae40c910bacf40b6c0916
khushisahoo <khushisahoo0246@gmail.com>
2022-01-27 23:07:16 +0530

    create order actions and reducers

---
+++ frontend/src/actions/orderActions.js
@@ -0,0 +1,34 @@
+import { ORDER_CREATE_FAIL , ORDER_CREATE_REQUEST , ORDER_CREATE_SUCCESS } from "../constants/orderConstants";
+export const createOrder =(order) => async(dispatch , getState) =>{
+    try{
+       dispatch({
+           type: ORDER_CREATE_REQUEST
+       })
+        const {userLogin : {userInfo}} = getState();
+        console.log("user update profile requet");
+        console.log(userInfo);
+        
+       const config={
+           headers:{
+               'Content-Type': 'application/json',
+               Authorization : `Bearer ${userInfo.token}`
+           }
+       }
+       console.log("user token recived")
+       console.log(userInfo.token)
+       const {data} = await axios.post('/api/users/orders' , order , config);
+       dispatch({
+           type: ORDER_CREATE_SUCCESS,
+           payload: data
+       });
+       
+    }catch(error){
+     dispatch({
+         type: ORDER_CREATE_FAIL,
+         payload: error.response && error.response.data.message 
+         ? error.response.data.message 
+         : error.message
+     })
+ 
+    }
+ }
